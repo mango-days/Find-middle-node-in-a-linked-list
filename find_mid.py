7b86f12060ec5beb44ce55f6b010417ed6d3345d
mango-days <positivejasminekaur@gmail.com>
2021-12-03 23:33:51 +0530

    first commit

---
+++ find_mid.py
@@ -0,0 +1,55 @@
+# Find the middle of a given linked list
+
+# Given a singly linked list, find the middle of the linked list. For example, if the given linked list is 1->2->3->4->5 then the output should be 3. 
+
+# If there are even nodes, then there would be two middle nodes, we need to print the second middle element. For example, if given linked list is 1->2->3->4->5->6 then the output should be 4.
+
+class Node :
+    def __init__ ( self , data ) :
+        self.data = data
+        self.next = None
+
+class LinkedList :
+    def __init__ ( self ) :
+        self.head = None
+    
+    def printList ( self ) :
+        temp = self.head
+        while ( temp ) :
+            print ( temp.data )
+            temp = temp.next
+        
+    def add ( self , data ) :
+        if self.head != None :
+            last = self.head
+            end = self.head
+            while ( end ) : 
+                last = end
+                end = end.next
+            last.next = Node ( data )
+        else : self.head = Node ( data )
+    
+    def size ( self ) :
+        temp = self.head
+        n = 0
+        while ( temp ) :
+            n += 1
+            temp = temp.next
+        return n
+    
+    def midElement ( self ) :
+        n = self.size()
+        index = 0
+        temp = self.head
+        while ( index != int ( n / 2 ) ) :
+            temp = temp.next
+            index += 1
+        print ( temp.data )
+
+llist = LinkedList()
+llist.add ( 1 )
+llist.add ( 2 )
+llist.add ( 3 )
+llist.add ( 4 )
+llist.add ( 5 )
+llist.midElement()
